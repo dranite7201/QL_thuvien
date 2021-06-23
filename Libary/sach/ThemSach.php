23c042565250875088e4d1743bb43ff91260715d
nhuhao0901 <74343854+nhuhao0901@users.noreply.github.com>
2021-06-23 08:52:32 +0700

    Update ThemSach.php

--- Libary/sach/ThemSach.php
+++ Libary/sach/ThemSach.php
@@ -1,23 +1,29 @@
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <link rel="stylesheet" href="form.css" />
+    <link rel="stylesheet" href="style.css" />
 </head>
-
 <body>
 
-    <form method="post" action="xulyAddSach.php" class="form">
+    <form method="post" action="ThemSach.php" class="form">
 
-        <h2>Thêm Sách</h2>
-        Tên Sách: <input type="text" name="tensach" value="" required> <br>
-        Tác Giả: <input type="text" name="tacgia" value="" required> <br>
-        Tình Trạng: <input type="text" name="tinhtrang" value="" required /> <br>
-        Loại Sách: <input type="number_format" name="maloai" value="" required> <br><br>
-
-        <input type="submit" name="themsach" value="Thêm Sách" style="color: red; background-color: aquamarine;" />
-        <?php require 'xulyAddSach.php'; ?>
+        <h2 style="text-align: center;">Điền vào nội dung sau để thêm sách</h2>
+        Tên Sách: <input type="text" name="title" value="" required/>
+        Mã số sách:<input type="text" name="isbn" value="" required/>
+        Số trang:<input type="text" name="pageCount" value="" required/>
+        Ngày đăng:<input type="date" name="publishedDate" value="" required/> 
+        Nguồn:<input type="link" name="thumbnailUrl" value="" required/>
+        Mô tả:<input type="text" name="shortDescription" value="" required/>
+        Nội dung chi tiết:<br>
+            <textarea  name="longDescription" rows="5" cols="40%"></textarea><br>
+        Tình trạng: <select name="status" >
+           <option  value="PUBLISH" >PUBLISH</option>
+           <option value="PRIVATE">PRIVATE</option>
+        </select> </br> 
+        Tác Giả: <input type="text" name="author_name" value="" required> <br> 
+        <input type="submit" name="themsach" value="Thêm Sách" style=" background-color:red ;width: 20%;font-size:40;color:white;border-radius: 8px;" />
+        <?php require 'xulyAddSach.php';?>
     </form>
 
 </body>
