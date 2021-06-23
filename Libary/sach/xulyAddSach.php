714ba01064bb5d2b6dfd3cc53a106ab9f412235b
nhuhao0901 <74343854+nhuhao0901@users.noreply.github.com>
2021-06-23 08:53:20 +0700

    Update xulyAddSach.php

--- Libary/sach/xulyAddSach.php
+++ Libary/sach/xulyAddSach.php
@@ -1,56 +1,79 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
-include_once('DataProvider.php');
-
+/*include_once ('DataProvider.php');
 $sqlLoai = "SELECT * FROM categories";
 $dsLoai = DataProvider::ExecuteQuery($sqlLoai);
 $sqlSach = "SELECT * FROM books";
-$dsSach = DataProvider::ExecuteQuery($sqlSach);
-
+$dsSach = DataProvider::ExecuteQuery($sqlSach);*/
+$conn = mysqli_connect('localhost', 'root', '', 'data1') or die ('Lỗi kết nối'); mysqli_set_charset($conn, "utf8");
 // Dùng isset để kiểm tra Form
 if (isset($_POST['themsach'])) {
-    $tensach = trim($_POST['title']);
-    $tacgia = trim($_POST['author']);
-    $loai = trim($_POST['category']);
-    $tinhtrang = trim($_POST['status']);
-    //$hinh = trim($_POST['Hinh']);
-
-    if (empty($tensach)) {
-        array_push($errors, "Tên Sách bắt buộc có");
+    $title = trim($_POST['title']);
+    $isbn = trim($_POST['isbn']);
+    $pageCount= trim($_POST['pageCount']);
+    $publishedDate= trim($_POST['publishedDate']);
+    $thumbnailUrl= trim($_POST['thumbnailUrl']);
+    $shortDescription= trim($_POST['shortDescription']);
+    $longDescription= trim($_POST['longDescription']);
+    $status = trim($_POST['status']);
+    $author_name = trim($_POST['author_name']);
+    if (empty($title)) {
+        array_push($errors, "Name is required");
     }
-    if (empty($tacgia)) {
-        array_push($errors, "Tác giả bắt buộc có");
+    if (empty($isbn)) {
+        array_push($errors, "MaSach is required");
     }
-    if (empty($tinhtrang)) {
-        array_push($errors, "bắt buộc có");
+    if (empty($pageCount)) {
+        array_push($errors, "PageCount is required");
     }
-    if (empty($loai)) {
-        array_push($errors, "bắt buộc có");
+    if (empty($publishedDate)) {
+        array_push($errors, "Date is required");
     }
-
+    if (empty($thumbnailUrl)) {
+        array_push($errors, "Url is required");
+    }
+    if (empty($shortDescription)) {
+        array_push($errors, "Description is required");
+    }
+    if (empty($longDescription)) {
+        array_push($errors, "Ndung is required");
+    }
+    if (empty($status)) {
+        array_push($errors, "Status is required");
+    }
+    if (empty($author_name)) {
+        array_push($errors, "Status is required");
+    }
     // Kiểm tra có trùng tên sách ko
-    $sql = "SELECT * FROM books WHERE title = '$tensach'";
-
+    $sql = "SELECT * FROM books WHERE title = '$title'";
     // Thực thi câu truy vấn
     $result = mysqli_query($conn, $sql);
-
-    // Nếu kết quả trả về lớn hơn 1 thì nghĩa là username hoặc email đã tồn tại trong CSDL
-    if (mysqli_num_rows($result) > 0) {
-        echo '<script language="javascript">alert("Bị trùng tên sách hoặc chưa nhập tên sách!"); window.location="register.php";</script>';
-
+    if (mysqli_num_rows($result) > 0) 
+    {
+        echo '<script language="javascript">alert("Bị trùng tên sách hoặc chưa nhập tên sách!"); window.location="ThemSach.php";</script>';
         // Dừng chương trình
         die();
-    } else {
-        $sql = "INSERT INTO books (title, author, status, category) VALUES ('$tensach','$tacgia','$tinhtrang','$loai')";
+    } 
+    else {
+        $sql = "INSERT INTO books (title,isbn,pageCount,publishedDate,thumbnailUrl,shortDescription,longDescription, status) VALUES ('$title','$isbn','$pageCount','$publishedDate','$thumbnailUrl','$shortDescription','$longDescription','$status')";
+        echo '<script language="javascript">alert("Thêm sách thành công!"); window.location="ThemSach.php";</script>';
+        $sql = "INSERT INTO authors (author_name) VALUES ('$author_name')";
         echo '<script language="javascript">alert("Thêm sách thành công!"); window.location="ThemSach.php";</script>';
 
         if (mysqli_query($conn, $sql)) {
             echo "Tên Sách: " . $_POST['title'] . "<br/>";
-            echo "Tác Giả: " . $_POST['author'] . "<br/>";
+            echo "Mã số sách: " . $_POST['isbn'] . "<br/>";
+            echo "Số trang: " . $_POST['pageCount'] . "<br/>";
+            echo "Nguồn: " . $_POST['thumbnaillUrl'] . "<br/>";
+            echo "Ngày đăng: " . $_POST['publishedDate'] . "<br/>";
+            echo "Mô tả " . $_POST['shortDescription'] . "<br/>";
+            echo "Nội dung: " . $_POST['longDescription'] . "<br/>";
             echo "Tình Trạng Sách: " . $_POST['status'] . "<br/>";
-            echo "Loại : " . $_POST['category'] . "<br/>";
+            echo "Tác Giả: " . $_POST['author_name'] . "<br/>";
+
         } else {
             echo '<script language="javascript">alert("Có lỗi trong quá trình xử lý"); window.location="ThemSach.php";</script>';
         }
     }
 }
+?>
