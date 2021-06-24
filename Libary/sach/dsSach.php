0d520b4d299ff3f89ffcca30ab98d910833a5589
Dong Nguyen <62019140+dranite7201@users.noreply.github.com>
2021-06-24 22:38:31 +0700

    Update dsSach.php

--- Libary/sach/dsSach.php
+++ Libary/sach/dsSach.php
@@ -4,7 +4,8 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <link rel="stylesheet" href="./style.css" />
+    <link rel="stylesheet" href="style-sach.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
 <body>
@@ -64,8 +65,12 @@
                         Tác giả: {$sach['author_name']}
                         </div>
                         <div class="hh-box-sua">
-                    <a href ="SuaInfoSach.php?id=<?php echo {$sach['id']}; ?>">Sửa</a>
-                    </div>
+                            <a href ="SuaInfoSach.php?id=<?php echo {$sach['id']}; ?>">Sửa</a>
+                        </div>
+                        <form method="post" action="xoasach.php" style="display: inline-block">
+                            <input  type="hidden" name="id" value="<?php echo {$sach['id']} ?>"/>
+                            <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
+                        </form>
                     </div>
                 EOD;
                 echo $dsSach;
