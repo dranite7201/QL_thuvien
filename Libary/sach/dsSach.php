c0438a260f35e173d2ce2f44c584a40530cb25d4
ManTT-12 <74489008+ManTT-12@users.noreply.github.com>
2021-06-26 15:00:10 +0700

    sach
    để mở thư viện trang index.php ALL BOOKS

--- Libary/sach/dsSach.php
+++ Libary/sach/dsSach.php
@@ -1,5 +1,5 @@
 <?php
-//ĐỂ NGOÀI THƯ VIỆN index.html ALLBOOKS
+
 $pdo = new PDO('mysql:host=localhost;port=3306;dbname=data1', 'root', '');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
@@ -28,34 +28,34 @@ $categories = $statement1->fetchAll(PDO::FETCH_ASSOC);
 
 <body>
     <div>
-        <a style="float: right; color: black" href="../user.html">Đăng Nhập</a>
-        <h1 style="text-align: center; color:brown">LIST BOOKS</h1>
+        <a style="float: right; color: black" href="../user.php">Đăng Nhập</a>
+        <h1 style="text-align: center; color:white; background-color: crimson;">LIST BOOKS</h1>
     </div>
     </p>
     <table class="table">
         <thead>
             <tr>
-                <th scope="col">#</th>
-                <th scope="col">Hình ảnh</th>
-                <th scope="col">Tên Sách</th>
-                <th scope="col">Tác Giả</th>
-                <th scope="col">Thế loại</th>
-                <th scope="col">Mô tả </th>
+                <th style="border-color:firebrick" scope="col">#</th>
+                <th style="border-color:firebrick" scope="col">Hình ảnh</th>
+                <th style="border-color:firebrick" scope="col">Tên Sách</th>
+                <th style="border-color:firebrick" scope="col">Tác Giả</th>
+                <th style="border-color:firebrick" scope="col">Thế loại</th>
+                <th style="border-color:firebrick" scope="col">Mô tả </th>
             </tr>
         </thead>
         <tbody>
             <?php foreach ($books as $i => $book) { ?>
                 <tr>
-                    <th scope="row"><?php echo $i + 1 ?></th>
-                    <td>
+                    <th style="border-color:firebrick" scope="row"><?php echo $i + 1 ?></th>
+                    <td style="border-color:firebrick">
                         <?php if ($book['thumbnailUrl']) : ?>
                             <img src="<?php echo $book['thumbnailUrl'] ?>" alt="<?php echo $book['title'] ?>" class="product-img">
                         <?php endif; ?>
                     </td>
-                    <td><?php echo $book['title'] ?></td>
-                    <td><?php echo $book['author_name'] ?></td>
-                    <td><?php echo $book['category'] ?></td>
-                    <td><?php echo $book["shortDescription"] ?></td>
+                    <td style="border-color:firebrick"><?php echo $book['title'] ?></td>
+                    <td style="border-color:firebrick"><?php echo $book['author_name'] ?></td>
+                    <td style="border-color:firebrick"><?php echo $book['category'] ?></td>
+                    <td style="border-color:firebrick"><?php echo $book["shortDescription"] ?></td>
                 </tr>
             <?php } ?>
         </tbody>
