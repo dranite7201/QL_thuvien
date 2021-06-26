ded350808bfa65199bd92875afeebc8997442629
ManTT-12 <74489008+ManTT-12@users.noreply.github.com>
2021-06-26 15:03:42 +0700

    sach - TRANG ADMIN DÙNG ĐỂ CHỨA DSSACHADMIN.PHP
    CÓ THỂ THÊM NHỮNG CÁI KHÁC ĐỂ HOÀN CHỈNH HƠN

---
+++ Libary/sach/admin.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css" />
+</head>
+
+<body>
+    <main>
+        <div>
+            <a style="float:right" href="../dangnhap/dangxuat.php">Đăng Xuất</a>
+            <h1 class="section-heading" style="background-color: crimson; color:white; border-color:black">Trang ADMIN</h1>
+        </div>
+        <div id="right-footer">
+            <button style="font-size:large;background-color:white;font-family:Time new roman;text-align:center" class="btn btn-sm btn-success"><a href="./dsSachAdmin.php">ALL BOOKS </a></button>
+            <button style="font-size:large;background-color:white;font-family:Time new roman;text-align:center" class="btn btn-sm btn-success"><a href="./ThemSach.php">THÊM SÁCH </a></button>
+            <button style="font-size:large;background-color:white;font-family:Time new roman;text-align:center" class="btn btn-sm btn-success"><a href="./SuaInfoSach.php">SỬA THÔNG TIN SÁCH </a></button>
+        </div>
+
+    </main>
+</body>
+
+</html>
