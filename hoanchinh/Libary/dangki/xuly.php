c9e36b87192f2ce6b1b2e6a0424a6dfb6fdcac5a
ManTT-12 <74489008+ManTT-12@users.noreply.github.com>
2021-07-09 20:33:11 +0700

    Add files via upload

--- hoanchinh/Libary/dangki/xuly.php
+++ hoanchinh/Libary/dangki/xuly.php
@@ -8,7 +8,7 @@ mysqli_set_charset($conn, "utf8");
 if (isset($_POST['dangky'])) {
     $hoten = trim($_POST['hoten']);
     $username = trim($_POST['username']);
-    $password = trim($_POST['password']);
+    $password = trim($_POST['password']);   
     $ngsinh = trim($_POST['ngsinh']);
     $email = trim($_POST['email']);
     $phone = trim($_POST['phone']);
@@ -41,23 +41,23 @@ if (isset($_POST['dangky'])) {
 
     // Nếu kết quả trả về lớn hơn 0 thì nghĩa là username hoặc email đã tồn tại trong CSDL
     if (mysqli_num_rows($result) > 0) {
-        echo '<script language="javascript">alert("Bị trùng tên hoặc chưa nhập tên!"); window.location="dangky.php";</script>';
+        echo '<script language="javascript">alert("Bị trùng tên hoặc chưa nhập tên!"); window.location="../user.html";</script>';
 
         // Dừng chương trình
         die();
     } else {
         $sql = "INSERT INTO thanhvien (username, password, hoten, ngsinh, email, phone) VALUES ('$username','$password','$hoten','$ngsinh','$email','$phone')";
-        echo '<script language="javascript">alert("Đăng ký thành công!"); window.location="user.html";</script>';
+        echo '<script language="javascript">alert("Đăng ký thành công!"); window.location="../user.html";</script>';
 
         if (mysqli_query($conn, $sql)) {
-            echo "Họ và tên: " . $_POST['hoten'] . "<br/>";
+            echo "Họ và tên: " . $_POST['hoten'] . "<br/>"; 
             echo "Tên đăng nhập: " . $_POST['username'] . "<br/>";
             echo "Mật khẩu: " . $_POST['password'] . "<br/>";
             echo "Ngày sinh: " . $_POST['ngsinh'] . "<br/>";
             echo "Email đăng nhập: " . $_POST['email'] . "<br/>";
             echo "Số điện thoại: " . $_POST['phone'] . "<br/>";
         } else {
-            echo '<script language="javascript">alert("Có lỗi trong quá trình xử lý"); window.location="user.html";</script>';
+            echo '<script language="javascript">alert("Có lỗi trong quá trình xử lý"); window.location="../user.html";</script>';
         }
     }
 }
